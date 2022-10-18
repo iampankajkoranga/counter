df3de31cbe9d1f2ffa0963d9779d9a33b2c9fed6
Pankaj Singh <pankajsingh@TTNs-MacBook-Pro.local>
2022-10-18 11:21:40 +0530

    stable code

--- Navigate.js
+++ Navigate.js
@@ -1,25 +1,25 @@
 // In App.js in a new project
 
-import * as React from 'react';
-import {View, Text, SafeAreaView} from 'react-native';
-import {NavigationContainer, StackActions} from '@react-navigation/native';
+import {Component} from 'react';
+//import {View, Text, SafeAreaView} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import MovieTicket from './MovieTicket';
-import Otp from './Otp';
+import OtpScreen from './OtpScreen';
 import App from './App';
 import MainScreen from './MainScreen';
 
 const Stack = createNativeStackNavigator();
 
 
-export default class Navigate extends React.Component {
+export default class Navigate extends Component {
   render() {
     return (
       <NavigationContainer>
         <Stack.Navigator>
           <Stack.Screen name="Home" component={MainScreen} />
           <Stack.Screen name="Movie" component={MovieTicket} />
-          <Stack.Screen name="otp" component={Otp} />
+          <Stack.Screen name="otp" component={OtpScreen} />
           <Stack.Screen name="Counter" component={App} />
         </Stack.Navigator>
       </NavigationContainer>
--- index.js
+++ index.js
@@ -5,9 +5,10 @@
 import {AppRegistry} from 'react-native';
 // import App from './App';
 // import Login from './Login';
-import MovieTicket from './MovieTicket';
+//import MovieTicket from './MovieTicket';
+import Navigate from './Navigate';
 
 import {name as appName} from './app.json';
 
 // AppRegistry.registerComponent(appName, () => Login);
-AppRegistry.registerComponent(appName, () => MovieTicket);
+AppRegistry.registerComponent(appName, () =>  Navigate);
